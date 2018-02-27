c05ad7c983653d2a10a2efcd5ba6e28f494710f8
Jesse Riggins <jdriggin@umail.iu.edu>
2018-02-27 14:50:02 -0500

    Completes tutorial 5.

---
+++ Basic Training/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Collections.Generic;       //Allows us to use Lists. 
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+    private BoardManager boardScript;
+    private int level = 3;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+
+    }
+
+
+
+    //Update is called every frame.
+    void Update()
+    {
+
+    }
+}
