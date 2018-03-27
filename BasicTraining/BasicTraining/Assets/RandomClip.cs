a713cdbac059a1ee5d54e6afaf4fc6c295200658
Professor Enigma <theprofessorenigma@gmail.com>
2018-03-27 06:28:20 -0400

    Cadences will play.

---
+++ BasicTraining/BasicTraining/Assets/RandomClip.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomClip : MonoBehaviour {
+    public List<AudioClip> clips;
+    public AudioSource source;
+
+    // Use this for initialization
+    void Start () {
+        if (clips.Count > 0 && source != null)
+        {
+            source.clip = clips[Random.Range(0, clips.Count)];
+            //Debug.Log(source.clip.name);
+            source.Play();
+        }   
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
